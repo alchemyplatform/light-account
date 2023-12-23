0b70c8e28658d0254a29e944dbf109eee1a6f04c
howydev <132113803+howydev@users.noreply.github.com>
2023-12-23 16:21:41 -0500

    chore: update deploy script

--- script/Deploy_LightAccountFactory.s.sol
+++ script/Deploy_LightAccountFactory.s.sol
@@ -7,7 +7,7 @@ import {IEntryPoint} from "account-abstraction/interfaces/IEntryPoint.sol";
 
 import {LightAccountFactory} from "../src/LightAccountFactory.sol";
 
-// @notice Deploys LightAccountFactory to the address `0x00000055C0b4fA41dde26A74435ff03692292FBD`
+// @notice Deploys LightAccountFactory to the address `[TODO]`
 // @dev Note: Script uses EntryPoint at address 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789
 // @dev To run: `forge script script/Deploy_LightAccountFactory.s.sol:Deploy_LightAccountFactory --broadcast --rpc-url ${RPC_URL} --verify -vvvv`
 contract Deploy_LightAccountFactory is Script {
@@ -18,7 +18,7 @@ contract Deploy_LightAccountFactory is Script {
         vm.startBroadcast();
 
         // Using entryPoint: 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789
-        // Correct as of Oct 3 2023, from https://docs.alchemy.com/reference/eth-supportedentrypoints
+        // Correct as of Dec 23 2023, from https://docs.alchemy.com/reference/eth-supportedentrypoints
         IEntryPoint entryPoint = IEntryPoint(payable(0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789));
 
         // Init code hash check
@@ -26,7 +26,7 @@ contract Deploy_LightAccountFactory is Script {
             abi.encodePacked(type(LightAccountFactory).creationCode, bytes32(uint256(uint160(address(entryPoint)))))
         );
 
-        if (initCodeHash != 0x2ad62a8bb3850247ef0c4f04e30b584e6eee7caa0e063745e90956653b90eb84) {
+        if (initCodeHash != 0x72db05fba86adbc64040d8949a885cd2a59aa1604d3c84bec19870bd9a7db0d5) {
             revert InitCodeHashMismatch(initCodeHash);
         }
 
@@ -40,11 +40,11 @@ contract Deploy_LightAccountFactory is Script {
         console.log("********************************");
 
         LightAccountFactory factory = new LightAccountFactory{
-            salt: 0x4e59b44847b379578588920ca78fbf26c0b4956c3406f3bdc271500000c2f72f
+            salt: 0x4e59b44847b379578588920ca78fbf26c0b4956cce883a81e259e4000035f09a
         }(entryPoint);
 
         // Deployed address check
-        if (address(factory) != 0x00000055C0b4fA41dde26A74435ff03692292FBD) {
+        if (address(factory) != 0x00000010A100a25D176503d5c50C4ae0dF2d5120) {
             revert DeployedAddressMismatch(address(factory));
         }
 
