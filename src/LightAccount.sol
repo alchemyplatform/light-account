9e0349642ccbbbb3336f71afc15bcf53844e4baf
Dan <dan.coombs@alchemy.com>
2026-01-19 15:55:50 -0600

    WIP: feat: add an EIP-7702 impl of single owner LA

--- src/LightAccount.sol
+++ src/LightAccount.sol
@@ -1,14 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.28;
 
-import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import {SignatureChecker} from "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
-import {SIG_VALIDATION_FAILED} from "account-abstraction/core/Helpers.sol";
 import {IEntryPoint} from "account-abstraction/interfaces/IEntryPoint.sol";
 import {PackedUserOperation} from "account-abstraction/interfaces/PackedUserOperation.sol";
 
-import {BaseLightAccount} from "./common/BaseLightAccount.sol";
-import {CustomSlotInitializable} from "./common/CustomSlotInitializable.sol";
+import {SingleOwnerLightAccountBase} from "./SingleOwnerLightAccountBase.sol";
 
 /// @title A simple ERC-4337 compatible smart contract account with a designated owner account.
 /// @dev Like eth-infinitism's SimpleAccount, but with the following changes:
@@ -32,21 +28,7 @@ import {CustomSlotInitializable} from "./common/CustomSlotInitializable.sol";
 /// 4. Event `SimpleAccountInitialized` renamed to `LightAccountInitialized`.
 ///
 /// 5. Uses custom errors.
-contract LightAccount is BaseLightAccount, CustomSlotInitializable {
-    using ECDSA for bytes32;
-
-    /// @dev The version used for namespaced storage is not linked to the release version of the contract. Storage
-    /// versions will be updated only when storage layout changes are made.
-    /// keccak256(abi.encode(uint256(keccak256("light_account_v1.storage")) - 1)) & ~bytes32(uint256(0xff));
-    bytes32 internal constant _STORAGE_POSITION = 0x691ec1a18226d004c07c9f8e5c4a6ff15a7b38db267cf7e3c945aef8be512200;
-    /// @dev keccak256(abi.encode(uint256(keccak256("light_account_v1.initializable")) - 1)) & ~bytes32(uint256(0xff));
-    bytes32 internal constant _INITIALIZABLE_STORAGE_POSITION =
-        0x33e4b41198cc5b8053630ed667ea7c0c4c873f7fc8d9a478b5d7259cec0a4a00;
-
-    struct LightAccountStorage {
-        address owner;
-    }
-
+contract LightAccount is SingleOwnerLightAccountBase {
     /// @notice Emitted when this account is first initialized.
     /// @param entryPoint The entry point.
     /// @param owner The initial owner.
@@ -61,10 +43,7 @@ contract LightAccount is BaseLightAccount, CustomSlotInitializable {
     /// @dev The new owner is not a valid owner (e.g., `address(0)`, the account itself, or the current owner).
     error InvalidOwner(address owner);
 
-    constructor(IEntryPoint entryPoint_) CustomSlotInitializable(_INITIALIZABLE_STORAGE_POSITION) {
-        _ENTRY_POINT = entryPoint_;
-        _disableInitializers();
-    }
+    constructor(IEntryPoint entryPoint) SingleOwnerLightAccountBase(entryPoint) {}
 
     /// @notice Called once as part of initialization, either during initial deployment or when first upgrading to
     /// this contract.
@@ -88,7 +67,7 @@ contract LightAccount is BaseLightAccount, CustomSlotInitializable {
 
     /// @notice Return the current owner of this account.
     /// @return The current owner.
-    function owner() public view returns (address) {
+    function owner() public view override returns (address) {
         return _getStorage().owner;
     }
 
@@ -112,6 +91,7 @@ contract LightAccount is BaseLightAccount, CustomSlotInitializable {
     }
 
     /// @dev Implement template method of BaseAccount.
+    /// Supports both EOA signatures and ERC-1271 contract signatures.
     function _validateSignature(PackedUserOperation calldata userOp, bytes32 userOpHash)
         internal
         virtual
@@ -130,24 +110,6 @@ contract LightAccount is BaseLightAccount, CustomSlotInitializable {
             return _successToValidationData(_isValidContractOwnerSignatureNow(userOpHash, signature));
         }
         revert InvalidSignatureType();
-    }
-
-    /// @notice Check if the signature is a valid by the EOA owner for the given digest.
-    /// @dev Only supports 65-byte signatures, and uses the digest directly. Reverts if the signature is malformed.
-    /// @param digest The digest to be checked.
-    /// @param signature The signature to be checked.
-    /// @return True if the signature is valid and by the owner, false otherwise.
-    function _isValidEOAOwnerSignature(bytes32 digest, bytes memory signature) internal view returns (bool) {
-        address recovered = digest.recover(signature);
-        return recovered == owner();
-    }
-
-    /// @notice Check if the signature is a valid ERC-1271 signature by a contract owner for the given digest.
-    /// @param digest The digest to be checked.
-    /// @param signature The signature to be checked.
-    /// @return True if the signature is valid and by an owner, false otherwise.
-    function _isValidContractOwnerSignatureNow(bytes32 digest, bytes memory signature) internal view returns (bool) {
-        return SignatureChecker.isValidERC1271SignatureNow(owner(), digest, signature);
     }
 
     /// @dev The signature is valid if it is signed by the owner's private key (if the owner is an EOA) or if it is a
@@ -185,16 +147,5 @@ contract LightAccount is BaseLightAccount, CustomSlotInitializable {
         name = "LightAccount";
         // Set to the major version of the GitHub release at which the contract was last updated.
         version = "2";
-    }
-
-    function _isFromOwner() internal view virtual override returns (bool) {
-        return msg.sender == owner();
-    }
-
-    function _getStorage() internal pure returns (LightAccountStorage storage storageStruct) {
-        bytes32 position = _STORAGE_POSITION;
-        assembly ("memory-safe") {
-            storageStruct.slot := position
-        }
     }
 }
